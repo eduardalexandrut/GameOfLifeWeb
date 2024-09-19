3e6e3ec0123e12f5f7738368997438f329107bce
eduardalexandrut <aeduardtoni@gmail.com>
2024-09-19 21:38:10 +0200

    Modified /get-worlds endpoint, in order to loop trough all the files and load all the worlds.

--- game-of-life/server/server.js
+++ game-of-life/server/server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { json } = require('stream/consumers');
 const app = express();
 const port = 5000;
 
@@ -56,65 +57,47 @@ app.post('/add-world', (req, res) => {
     }
   });
 });
-/*app.post('/add-world', (req, res) => {
-  console.log('Received data:', req.body); 
-  const filePath = path.join(__dirname, '../data/data.json'); // Absolute path to the JSON file
-  const new_world = req.body; // JSON object to be added
 
-  // Read the current data from the file
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      console.error(`Error reading file: ${err}`);
-      return res.status(500).send(`Error when reading the file: ${err}`);
-    }
-
-    let jsonArray;
-    try {
-      jsonArray = JSON.parse(data);
-      if (!Array.isArray(jsonArray)) {
-        throw new Error('JSON data is not an array');
-      }
-    } catch (parseError) {
-      return res.status(500).send(`Error parsing JSON data: ${parseError}`);
-    }
-    console.log(jsonArray)
-
-    // Add the new world to the array
-    if (!jsonArray.includes(new_world)){
-      jsonArray.push(new_world);
-    } else {
-      jsonArray.map((world) => world.id === new_world.id ? new_world : world)
-    }
-
-    // Write the updated array back to the file
-    fs.writeFile(filePath, JSON.stringify(jsonArray, null, 2), 'utf-8', (writeError) => {
-      if (writeError) {
-        return res.status(500).send(`Error writing to file: ${writeError}`);
-      }
-      res.status(200).send('World successfully added to the database.');
-    });
-  });
-});*/
 
 app.get('/get-worlds', (req, res) => {
-  const fileName = path.join(__dirname, "../data/data.json");
+  const directoryPath = path.join(__dirname, '../data');
 
-  fs.readFile(fileName, 'utf-8', (err, data) => {
+  fs.readdir(directoryPath, (err, files) => {
     if (err) {
-      return res.status(500).send(`Error when opening the file: ${err}`);
+      return res.status(500).send(`Unable to read directory: ${err}`);
     }
 
-    let jsonData;
-    try {
-      jsonData = JSON.parse(data);
-    } catch (parseErr) {
-      return res.status(500).send(`Error while parsing the JSON data: ${parseErr}`);
-    }
+    const jsonFiles = files.filter((file) => path.extname(file) === '.json');
 
-    res.json(jsonData); // Send the response only once
+    // Map over jsonFiles to create an array of promises for fs.readFile
+    const fileReadPromises = jsonFiles.map((file) => {
+      const filePath = path.join(directoryPath, file);
+
+      return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+          if (err) {
+            return reject(`Error reading file ${file}: ${err}`);
+          }
+          try {
+            const jsonData = JSON.parse(data);
+            resolve(jsonData);
+          } catch (parseError) {
+            reject(`Error parsing JSON in file ${file}: ${parseError}`);
+          }
+        });
+      });
+    });
+
+    // Wait for all file reads to finish
+    Promise.all(fileReadPromises)
+      .then((worlds) => {
+        res.status(200).json(worlds); // Send the collected worlds data
+      })
+      .catch((error) => {
+        res.status(500).send(error); // Handle any errors that occurred
+      });
   });
 });
-
 
 
 // Start the server
