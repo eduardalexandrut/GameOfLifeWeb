cc2b76f0056156db1c937dfd0013afcbda552d13
eduardalexandrut <aeduardtoni@gmail.com>
2024-09-21 16:09:34 +0200

    Added slider and text area component from shadcn.

--- game-of-life/src/components/WorldBuilder.tsx
+++ game-of-life/src/components/WorldBuilder.tsx
@@ -6,7 +6,8 @@ import React from "react";
 import {v4 as uuidv4} from 'uuid';
 import {Button} from './ui/Button';
 import { RadioGroup, RadioGroupItem } from "./ui/radio-group"
-
+import { Slider } from "./ui/slider";
+import { Textarea } from "./ui/textarea"
 
 
 type propType = {
